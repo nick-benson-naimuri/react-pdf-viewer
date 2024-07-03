c406cf6dc5c27593a3c0270ef104f89f10a57b1b
nick.benson <nick.benson@naimuri.com>
2024-07-03 14:31:52 +0100

    fix build issues

--- packages/scroll-mode/src/scrollModePlugin.tsx
+++ packages/scroll-mode/src/scrollModePlugin.tsx
@@ -23,8 +23,8 @@ import { SwitchScrollModeMenuItem } from './SwitchScrollModeMenuItem';
 import { SwitchViewMode, SwitchViewModeProps } from './SwitchViewMode';
 import { SwitchViewModeButton } from './SwitchViewModeButton';
 import { SwitchViewModeMenuItem } from './SwitchViewModeMenuItem';
-import { switchScrollMode } from './switchScrollMode';
-import { switchViewMode } from './switchViewMode';
+import { switchScrollMode } from './switchScrollModeFunction';
+import { switchViewMode } from './switchViewModeFunction';
 import { type StoreProps } from './types/StoreProps';
 
 export interface SwitchScrollModeButtonProps {
